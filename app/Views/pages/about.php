6d58a825d52a4bf57201c165b95d1265fe209a3a
Jose Pablo Navarro Salinas <josepablonavarro55@gmail.com>
2025-05-12 14:39:06 -0600

    cambios en home

---
+++ app/Views/pages/about.php
@@ -0,0 +1,2 @@
+<h1>Sobre nosotros</h1>
+<p>Esta es la página About. Aquí va información sobre nosotros.</p>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,2 @@
+<h1>¡Hola mundo desde Home!</h1>
+<p>Bienvenido a la página de inicio.</p>
